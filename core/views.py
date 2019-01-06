7bace35167e8047faa5dee7a1e563ce9c8b90b54
songhm <songhm@qssec.com>
2019-01-06 20:04:10 +0800

    How to Use JWT Authentication with Django REST Framework“

---
+++ core/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated 
+
+class HelloView(APIView):
+    permission_classes = (IsAuthenticated,) 
+
+    def get(self, request):
+        content = {'message': 'Hello, World!'}
+        return Response(content)
